c619da2231201dedb7bf5a0dc8c7b794f5aa53d3
cthadeufaria <crls.thadeu@gmail.com>
2021-07-19 13:40:32 -0300

    core folder name update

--- api.py
+++ api.py
@@ -1,1 +1,1 @@
-from app import app
+from core import app
--- core/routes.py
+++ core/routes.py
@@ -1,5 +1,7 @@
-from app import app
-from flask import render_template
+from flask.helpers import url_for
+from core import app
+from flask import render_template, flash, redirect
+from core.forms import LoginForm
 
 
 @app.route('/')
@@ -18,6 +20,16 @@ def index():
         'description' : '/info/<id> Parameters: id: info id to be deleted',
     }]
     return render_template('index.html' , greetings=None, msg=msg, endpoints=endpoints)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
 
 
 # # Create GET request
